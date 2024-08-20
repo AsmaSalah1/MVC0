f7ce69b7a9fa682673ca08c7707b54a9aecea5fb
AsmaSalah1 <146439635+AsmaSalah1@users.noreply.github.com>
2024-08-20 23:19:50 +0300

    Add files via upload

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC0.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
